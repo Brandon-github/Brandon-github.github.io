3c4d334b93872754941af6670d1d5417a12e34cc
Brandon Alexis Machado Ambuila <brandonmachado159@gmail.com>
2021-03-03 12:33:45 -0500

    se realiza prueba de pagina web github

---
+++ js/toogle_event.js
@@ -0,0 +1,8 @@
+
+function Toogle(){
+    const toogle = document.querySelector('.nav__toogle');
+    const barra = document.querySelector('.nav__links ul');
+    toogle.classList.toggle('active');
+    barra.classList.toggle('active');
+    
+}
